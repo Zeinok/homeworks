85df64966f269d87c491678f27adc496a493eff1
Zeinok <zeinok@protonmail.com>
2020-03-23 03:08:47 +0800

    Finish homework

---
+++ ShiftingEncrypt/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace ShiftingEncrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonOpen.Location = new System.Drawing.Point(12, 12);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(120, 51);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "開啟檔案";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(144, 75);
+            this.Controls.Add(this.buttonOpen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "字元位移";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpen;
+    }
+}
+
---
+++ ShiftingEncrypt/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace ShiftingEncrypt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog
+            {
+                Filter = "*.txt|*.txt|*.*|*.*"
+            };
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+
+            var sfd = new SaveFileDialog
+            {
+                Filter = "*.txt|*.txt|*.*|*.*"
+            };
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            var s = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            var os = new FileStream(sfd.FileName, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read);
+            while (s.Position<s.Length)
+            {
+                char c = (char)s.ReadByte();
+                if (c >= 'A' && c <= 'Z' ||
+                    c >= 'a' && c <= 'z' ||
+                    c >= '0' && c <= '9')
+                {
+                    byte cc = (byte)c;
+                    cc += 5;
+                    if(c >= '0' && c <= '9' && cc > '9')
+                    {
+                        cc -= 10;
+                    }
+                    if (c >= 'A' && c <= 'Z' && cc > 'Z')
+                    {
+                        cc -= 26;
+                    }
+                    if (c >= 'a' && c <= 'z' && cc > 'z')
+                    {
+                        cc -= 26;
+                    }
+                    os.WriteByte(cc);
+                }
+                else os.WriteByte((byte)c);
+            }
+            os.Close();
+            s.Close();
+            MessageBox.Show("處理成功。", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
