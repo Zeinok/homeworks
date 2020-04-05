5d59912ae5a740c91adac9a9eee66fbe07fb7d63
Zeinok <zeinok@protonmail.com>
2020-04-05 17:23:58 +0800

    Fix asciitable.c, add manual
    Remove third and forth row of asciitable.c

--- asciitable/asciitable.c
+++ asciitable/asciitable.c
@@ -4,12 +4,13 @@ const char ctrlChars[][4] = {"NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "B
 
 int main(){
     int i;
-    for(i=0;i<128/4;++i){
+    for(i=0;i<128/2;++i){
         char row1[4];
 	char* row1Ptr = row1;
 	row1[0] = i;
 	row1[1] = NULL;
 	if(i<32) row1Ptr = ctrlChars[i];
+
 	
 	char row2[4];
 	char* row2Ptr = row2;
@@ -17,10 +18,7 @@ int main(){
 	row2[1] = NULL;
 	if(64+i==127) row2Ptr = ctrlChars[32];
 
-	int row3 = 128+i;
-	if(row3==129||row3==141||row3==143||row3==144||row3==157||row3==160) row3 = '?'; // character doesn't exist
-
-        printf("| %3d %3x %3s | %3d %3x %3s | %3d %3x %3c | %3d %3x %3c |\n", i, i, row1Ptr, 64+i, 64+i, row2Ptr, 128+i, 128+i, row3, 192+i, 192+i, 192+i);
+        printf("| %3d %3x %3s | %3d %3x %3s |\n", i, i, row1Ptr, 64+i, 64+i, row2Ptr);
     }
     return 0;
 }
