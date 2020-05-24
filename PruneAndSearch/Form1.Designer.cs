6016e40d2e3392b54bf57b9cb1998ede49c519c6
Zeinok <zeinok@protonmail.com>
2020-05-24 22:20:59 +0800

    Finish prune and search

---
+++ PruneAndSearch/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace PruneAndSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonOpen = new System.Windows.Forms.ToolStripButton();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.labelK = new System.Windows.Forms.Label();
+            this.comboBoxK = new System.Windows.Forms.ComboBox();
+            this.labelP = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.buttonStep = new System.Windows.Forms.Button();
+            this.comboBoxP = new System.Windows.Forms.ComboBox();
+            this.labelEqu = new System.Windows.Forms.Label();
+            this.comboBoxPval = new System.Windows.Forms.ComboBox();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonOpen});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonOpen
+            // 
+            this.toolStripButtonOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonOpen.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonOpen.Image")));
+            this.toolStripButtonOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonOpen.Name = "toolStripButtonOpen";
+            this.toolStripButtonOpen.Size = new System.Drawing.Size(80, 22);
+            this.toolStripButtonOpen.Text = "開啟檔案 (&O)";
+            this.toolStripButtonOpen.Click += new System.EventHandler(this.toolStripButtonOpen_Click);
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxLog.Location = new System.Drawing.Point(10, 10);
+            this.textBoxLog.Margin = new System.Windows.Forms.Padding(10);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLog.Size = new System.Drawing.Size(780, 331);
+            this.textBoxLog.TabIndex = 1;
+            this.textBoxLog.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // labelK
+            // 
+            this.labelK.AutoSize = true;
+            this.labelK.Location = new System.Drawing.Point(8, 11);
+            this.labelK.Name = "labelK";
+            this.labelK.Size = new System.Drawing.Size(13, 12);
+            this.labelK.TabIndex = 2;
+            this.labelK.Text = "K";
+            // 
+            // comboBoxK
+            // 
+            this.comboBoxK.FormattingEnabled = true;
+            this.comboBoxK.Location = new System.Drawing.Point(27, 8);
+            this.comboBoxK.Name = "comboBoxK";
+            this.comboBoxK.Size = new System.Drawing.Size(50, 20);
+            this.comboBoxK.TabIndex = 3;
+            // 
+            // labelP
+            // 
+            this.labelP.AutoSize = true;
+            this.labelP.Location = new System.Drawing.Point(83, 11);
+            this.labelP.Name = "labelP";
+            this.labelP.Size = new System.Drawing.Size(11, 12);
+            this.labelP.TabIndex = 4;
+            this.labelP.Text = "P";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(10);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.comboBoxPval);
+            this.splitContainer1.Panel1.Controls.Add(this.labelEqu);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonStep);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBoxP);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBoxK);
+            this.splitContainer1.Panel1.Controls.Add(this.labelK);
+            this.splitContainer1.Panel1.Controls.Add(this.labelP);
+            this.splitContainer1.Panel1.Margin = new System.Windows.Forms.Padding(10);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxLog);
+            this.splitContainer1.Panel2.Padding = new System.Windows.Forms.Padding(10);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 384);
+            this.splitContainer1.SplitterDistance = 30;
+            this.splitContainer1.SplitterWidth = 3;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // buttonStep
+            // 
+            this.buttonStep.Location = new System.Drawing.Point(229, 6);
+            this.buttonStep.Name = "buttonStep";
+            this.buttonStep.Size = new System.Drawing.Size(75, 23);
+            this.buttonStep.TabIndex = 7;
+            this.buttonStep.Text = "Step";
+            this.buttonStep.UseVisualStyleBackColor = true;
+            this.buttonStep.Click += new System.EventHandler(this.buttonStep_Click);
+            // 
+            // comboBoxP
+            // 
+            this.comboBoxP.FormattingEnabled = true;
+            this.comboBoxP.Location = new System.Drawing.Point(100, 8);
+            this.comboBoxP.Name = "comboBoxP";
+            this.comboBoxP.Size = new System.Drawing.Size(50, 20);
+            this.comboBoxP.TabIndex = 6;
+            this.comboBoxP.SelectedIndexChanged += new System.EventHandler(this.comboBoxP_SelectedIndexChanged);
+            // 
+            // labelEqu
+            // 
+            this.labelEqu.AutoSize = true;
+            this.labelEqu.Location = new System.Drawing.Point(156, 11);
+            this.labelEqu.Name = "labelEqu";
+            this.labelEqu.Size = new System.Drawing.Size(11, 12);
+            this.labelEqu.TabIndex = 8;
+            this.labelEqu.Text = "=";
+            // 
+            // comboBoxPval
+            // 
+            this.comboBoxPval.FormattingEnabled = true;
+            this.comboBoxPval.Location = new System.Drawing.Point(173, 7);
+            this.comboBoxPval.Name = "comboBoxPval";
+            this.comboBoxPval.Size = new System.Drawing.Size(50, 20);
+            this.comboBoxPval.TabIndex = 9;
+            this.comboBoxPval.SelectedIndexChanged += new System.EventHandler(this.comboBoxPval_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 409);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonOpen;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Label labelK;
+        private System.Windows.Forms.ComboBox comboBoxK;
+        private System.Windows.Forms.Label labelP;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ComboBox comboBoxP;
+        private System.Windows.Forms.Button buttonStep;
+        private System.Windows.Forms.ComboBox comboBoxPval;
+        private System.Windows.Forms.Label labelEqu;
+    }
+}
+
---
+++ PruneAndSearch/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruneAndSearch
+{
+    public partial class Form1 : Form
+    {
+        private List<int> elements = new List<int>();
+        private int nextK = 0;
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private class State
+        {
+            public int[] S1;
+            public int[] S2;
+            public int[] S3;
+            public Set SetLeft;
+            public bool Found = false;
+            public enum Set {
+                S1,
+                S2,
+                S3
+            }
+            public State(int[] S1, int[] S2, int[] S3, Set SetLeft) {
+                this.S1 = S1;
+                this.S2 = S2;
+                this.S3 = S3;
+                this.SetLeft = SetLeft;
+                this.Found = false;
+            }
+            public State(int[] S1, int[] S2, int[] S3, Set SetLeft, bool Found) {
+                this.S1 = S1;
+                this.S2 = S2;
+                this.S3 = S3;
+                this.SetLeft = SetLeft;
+                this.Found = Found;
+            }
+        }
+
+        private void toolStripButtonOpen_Click(object sender, EventArgs e) {
+            try {
+                var ofd = new OpenFileDialog();
+                if(ofd.ShowDialog() != DialogResult.OK) return;
+                elements.Clear();
+                var s = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                var sr = new StreamReader(s);
+                string text = sr.ReadToEnd();
+                foreach(var val in text.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)) {
+                    elements.Add(int.Parse(val));
+                }
+                comboBoxK.Enabled = true;
+                buttonStep.Enabled = true;
+                comboBoxK.Items.Clear();
+                comboBoxP.Items.Clear();
+                comboBoxPval.Items.Clear();
+                for(int i = 0; i < elements.Count; i++) {
+                    comboBoxK.Items.Add(i+1);
+                    comboBoxP.Items.Add(i+1);
+                    comboBoxPval.Items.Add(elements[i]);
+                }
+                comboBoxK.SelectedIndex = 0;
+                comboBoxP.SelectedIndex = 0;
+                comboBoxPval.SelectedIndex = 0;
+                textBoxLog.Clear();
+                StringBuilder sb = new StringBuilder("S = ");
+                sb.AppendFormat("{{{0}}}", string.Join(", ", elements.ToArray()));
+                appendText(sb.ToString());
+            } catch(Exception ex) {
+                MessageBox.Show(ex.ToString(), ex.Message);
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e) {
+            e.Handled = true;
+            
+        }
+
+        private void appendText(string s) {
+            textBoxLog.Text += s + "\r\n";
+            textBoxLog.SelectionStart = textBoxLog.TextLength;
+            textBoxLog.ScrollToCaret();
+
+        }
+
+        private void buttonStep_Click(object sender, EventArgs e) {
+            if(comboBoxK.Enabled) {
+                nextK = comboBoxK.SelectedIndex;
+                comboBoxK.Enabled = false;
+            }
+            appendText(new string('-', 10));
+            State s = pruneAndSearch_Step(nextK, (int)comboBoxPval.SelectedItem, elements);
+            appendText(string.Format("K = {0}, K' = {1} , P = {2}", comboBoxK.SelectedItem, nextK, comboBoxP.SelectedItem));
+            appendText(string.Format("S1 = {{{0}}} {1}", string.Join(", ", s.S1), s.SetLeft == State.Set.S1 ? "保留" : "刪除"));
+            appendText(string.Format("S2 = {{{0}}} {1}", string.Join(", ", s.S2), s.SetLeft == State.Set.S2 ? "保留" : "刪除"));
+            appendText(string.Format("S3 = {{{0}}} {1}", string.Join(", ", s.S3), s.SetLeft == State.Set.S3 ? "保留" : "刪除"));
+            comboBoxP.Items.Clear();
+            comboBoxPval.Items.Clear();
+            for(int i = 0; i < elements.Count; i++) {
+                comboBoxP.Items.Add(i + 1);
+                comboBoxPval.Items.Add(elements[i]);
+            }
+            if(s.SetLeft == State.Set.S3) {
+                nextK -= s.S1.Length + s.S2.Length;
+            }
+            if(comboBoxP.Items.Count > 0) {
+                comboBoxP.SelectedIndex = 0;
+                comboBoxPval.SelectedIndex = 0;
+            }
+            if(s.Found) {
+                appendText(string.Format("已找到第K小的值，K值 = {0}", s.S2[0]));
+                buttonStep.Enabled = false;
+            }
+        }
+
+        private State pruneAndSearch_Step(int k, int pVal, List<int> Set) {
+            List<int> lessThan = new List<int>();
+            List<int> equals = new List<int>();
+            List<int> largerThan = new List<int>();
+            foreach(var v in Set) {
+                if(v < pVal) lessThan.Add(v);
+                if(v == pVal) equals.Add(v);
+                if(v > pVal) largerThan.Add(v);
+            }
+            State s;
+            Set.Clear();
+            if(lessThan.Count >= k+1) {
+                s = new State(lessThan.ToArray(), equals.ToArray(), largerThan.ToArray(), State.Set.S1);
+                Set.AddRange(lessThan);
+            } else if(lessThan.Count + equals.Count >= k+1) {
+                s = new State(lessThan.ToArray(), equals.ToArray(), largerThan.ToArray(), State.Set.S2, true);
+            } else {
+                s = new State(lessThan.ToArray(), equals.ToArray(), largerThan.ToArray(), State.Set.S3);
+                Set.AddRange(largerThan);
+            }
+
+            return s;
+        }
+
+        private void comboBoxP_SelectedIndexChanged(object sender, EventArgs e) {
+            comboBoxPval.SelectedIndex = comboBoxP.SelectedIndex;
+        }
+
+        private void comboBoxPval_SelectedIndexChanged(object sender, EventArgs e) {
+            comboBoxP.SelectedIndex = comboBoxPval.SelectedIndex;
+        }
+
+    }
+}
