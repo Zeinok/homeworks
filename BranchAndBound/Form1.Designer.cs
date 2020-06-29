a41726e2ae861bf768a5a8288fd368b320285de7
Zeinok <zeinok@protonmail.com>
2020-06-30 02:57:06 +0800

    Checkpoint

---
+++ BranchAndBound/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace BranchAndBound
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonOpen = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonStep = new System.Windows.Forms.ToolStripButton();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonOpen,
+            this.toolStripButtonStep});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonOpen
+            // 
+            this.toolStripButtonOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonOpen.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonOpen.Image")));
+            this.toolStripButtonOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonOpen.Name = "toolStripButtonOpen";
+            this.toolStripButtonOpen.Size = new System.Drawing.Size(43, 22);
+            this.toolStripButtonOpen.Text = "Open";
+            this.toolStripButtonOpen.Click += new System.EventHandler(this.toolStripButtonOpen_Click);
+            // 
+            // toolStripButtonStep
+            // 
+            this.toolStripButtonStep.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonStep.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonStep.Image")));
+            this.toolStripButtonStep.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonStep.Name = "toolStripButtonStep";
+            this.toolStripButtonStep.Size = new System.Drawing.Size(37, 22);
+            this.toolStripButtonStep.Text = "Step";
+            this.toolStripButtonStep.Click += new System.EventHandler(this.toolStripButtonStep_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 425);
+            this.splitContainer1.SplitterDistance = 600;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(196, 425);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(7);
+            this.pictureBox1.Size = new System.Drawing.Size(600, 425);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Branch and bound";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonOpen;
+        private System.Windows.Forms.ToolStripButton toolStripButtonStep;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ BranchAndBound/Form1.cs
@@ -0,0 +1,203 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BranchAndBound
+{
+    public partial class Form1 : Form
+    {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        enum CurrentStatus
+        {
+            None,
+            FindLowestBound,
+            SubstractLowestBound,
+            FindMaximumSum,
+            MakeColRowInfinite,
+            Finish
+        }
+
+        float[,] TSPMatrix;
+        float[,] nextTSPMatrix;
+        float[] xLowestBound;
+        float[] yLowestBound;
+        Dictionary<int, int> solution = new Dictionary<int, int>();
+        CurrentStatus currentStatus = CurrentStatus.None;
+        Point coordinateReadyToRemove = new Point(-1, -1);
+
+        private void toolStripButtonOpen_Click(object sender, EventArgs e) {
+            var ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() != DialogResult.OK) return;
+            var s = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            var sr = new StreamReader(s, Encoding.GetEncoding(950), true);
+            richTextBox1.Clear();
+            appendLog("File opened.");
+            int w = 0, h = 0;
+            var content = sr.ReadToEnd();
+            string[] splittedContent = content.Split(new char[] { ';', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            h = splittedContent.Length;
+            w = splittedContent[0].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
+            TSPMatrix = new float[w, h];
+            xLowestBound = new float[w];
+            yLowestBound = new float[h];
+            for(int line=0; line<splittedContent.Length; line++) {
+                List<float> numbers = splittedContent[line]
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToList()
+                    .ConvertAll(
+                        (v) => float.Parse(v)
+                    );
+                int i = 0;
+                numbers.ForEach((v) => TSPMatrix[i++, line] = v);
+            }
+            appendLog("Finished parsing");
+            PaintTSPMatrix();
+            appendLog("Painted TSP Matrix");
+            currentStatus = CurrentStatus.FindLowestBound;
+            solution.Clear();
+        }
+
+        private void toolStripButtonStep_Click(object sender, EventArgs e) {
+            if(TSPMatrix == null) return;
+            switch(currentStatus) {
+                case CurrentStatus.None:
+                    break;
+                case CurrentStatus.FindLowestBound:
+                    FindLowestBound();
+                    break;
+                case CurrentStatus.SubstractLowestBound:
+                    SubstractLowestBound();
+                    break;
+                case CurrentStatus.FindMaximumSum:
+                    FindMaximumSum();
+                    break;
+                case CurrentStatus.MakeColRowInfinite:
+                    MakeColRowInfinite();
+                    break;
+                case CurrentStatus.Finish:
+                    return;
+            }
+            currentStatus = (CurrentStatus)((int)++currentStatus % (int)CurrentStatus.Finish);
+        }
+
+        private void appendLog(string s) {
+            richTextBox1.AppendText(string.Format("{0}\n", s));
+            richTextBox1.SelectionStart = richTextBox1.TextLength;
+        }
+        private void PaintTSPMatrix() {
+            const int cellSize = 32;
+            const float cellSizeF = cellSize;
+            Font cellFont = new Font(SystemFonts.DefaultFont.FontFamily, 12);
+            if(TSPMatrix == null) return;
+            List<int> skippedRow = new List<int>();
+            List<int> skippedCol = new List<int>();
+/*            for(int x = 0; x < TSPMatrix.GetLength(0); x++) {
+                if(Enumerable.Range(0, TSPMatrix.GetLength(1))
+                    .Select(
+                        (y) => TSPMatrix[x, y] == float.PositiveInfinity
+                    ).Count() == TSPMatrix.GetLength(1))
+                    skippedCol.Add(x);
+            }
+            for(int y = 0; y < TSPMatrix.GetLength(1); y++) {
+                if(Enumerable.Range(0, TSPMatrix.GetLength(0))
+                    .Select(
+                        (x) => TSPMatrix[x, y] == float.PositiveInfinity
+                    ).Count() == TSPMatrix.GetLength(1))
+                    skippedCol.Add(y);
+            }*/
+            var bmp = new Bitmap(TSPMatrix.GetLength(0) * cellSize + cellSize, TSPMatrix.GetLength(1) * cellSize + cellSize);
+            var g = Graphics.FromImage(bmp);
+            g.Clear(Color.White);
+            for(int y = 0; y < TSPMatrix.GetLength(1); y++) {
+                //if(skippedCol.Contains(y)) continue;
+                for(int x = 0; x < TSPMatrix.GetLength(0); x++) {
+                    //if(skippedRow.Contains(x)) continue;
+                    g.DrawRectangle(Pens.Black, new Rectangle(x * cellSize, y * cellSize, cellSize, cellSize));
+                    var sSize = g.MeasureString(TSPMatrix[x, y].ToString(), cellFont);
+                    g.DrawString(
+                        TSPMatrix[x, y].ToString(),
+                        cellFont,
+                        Brushes.Black,
+                        x * cellSizeF + cellSizeF / 2 - sSize.Width / 2,
+                        y * cellSizeF + cellSizeF / 2 - sSize.Height / 2
+                        );
+                }
+            }
+
+            pictureBox1.Image = new Bitmap(bmp);
+            bmp.Dispose();
+        }
+
+        private void FindLowestBound() {
+            float[,] tempTSPMatrix = (float[,])TSPMatrix.Clone();
+            for(int y = 0; y < tempTSPMatrix.GetLength(1); y++) {
+                yLowestBound[y] = Enumerable.Range(0, tempTSPMatrix.GetLength(0))
+                                    .Select(
+                                        (x) => tempTSPMatrix[x, y]
+                                    ).Min();
+                for(int x = 0; x < tempTSPMatrix.GetLength(0); x++)
+                    tempTSPMatrix[x, y] -= yLowestBound[y];
+            }
+
+            for(int x = 0; x < tempTSPMatrix.GetLength(0); x++) {
+                xLowestBound[x] = Enumerable.Range(0, tempTSPMatrix.GetLength(1))
+                                    .Select(
+                                        (y) => tempTSPMatrix[x, y]
+                                    ).Min();
+                for(int y = 0; y < tempTSPMatrix.GetLength(1); y++)
+                    tempTSPMatrix[x, y] -= yLowestBound[y];
+            }
+            nextTSPMatrix = tempTSPMatrix;
+        }
+        private void SubstractLowestBound() {
+            TSPMatrix = nextTSPMatrix;
+            nextTSPMatrix = null;
+        }
+        private void FindMaximumSum() {
+            var indexOfZero = GetIndex(TSPMatrix, 0);
+            List<float> values = new List<float>();
+            
+            foreach(var t in indexOfZero) {
+                values.Add(GetSumOfMinimumValue(TSPMatrix, t));
+            }
+        }
+        private void MakeColRowInfinite() {
+
+        }
+
+        List<Point> GetIndex(float[,] matrix, float value) {
+            List<Point> points = new List<Point>();
+            for(int y = 0; y < matrix.GetLength(1); y++) 
+                for(int x = 0; x < matrix.GetLength(0); x++) 
+                    points.Add(new Point(x, y));
+            return points;
+            
+        }
+        float GetSumOfMinimumValue(float[,] matrix, int x, int y) {
+            return
+                Enumerable.Range(0, matrix.GetLength(0))
+                                    .Select(
+                                        _x => matrix[_x, y]
+                ).Where(_x => x != _x).Min()
+                +
+                Enumerable.Range(0, matrix.GetLength(1))
+                                    .Select(
+                                        _y => matrix[x, _y]
+                ).Where(_y => y != _y).Min();
+
+        }
+        float GetSumOfMinimumValue(float[,] matrix, Point p) {
+            return GetSumOfMinimumValue(matrix, p.X, p.Y);
+        }
+}
