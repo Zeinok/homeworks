01158772597775df282765f7a5fd2c2003daa5f8
Zeinok <zeinok@protonmail.com>
2020-06-30 17:21:30 +0800

    Finish algorithm-5, add binary

--- BranchAndBound/Form1.cs
+++ BranchAndBound/Form1.cs
@@ -20,20 +20,20 @@ namespace BranchAndBound
 
         enum CurrentStatus
         {
-            None,
+            LoopStart,
             FindLowestBound,
             SubstractLowestBound,
             FindMaximumSum,
             MakeColRowInfinite,
             Finish
         }
-
+        float[,] originalTSPMatrix;
         float[,] TSPMatrix;
         float[,] nextTSPMatrix;
         float[] xLowestBound;
         float[] yLowestBound;
         Dictionary<int, int> solution = new Dictionary<int, int>();
-        CurrentStatus currentStatus = CurrentStatus.None;
+        CurrentStatus currentStatus = CurrentStatus.LoopStart;
         Point coordinateReadyToRemove = new Point(-1, -1);
 
         private void toolStripButtonOpen_Click(object sender, EventArgs e) {
@@ -51,7 +51,7 @@ namespace BranchAndBound
             TSPMatrix = new float[w, h];
             xLowestBound = new float[w];
             yLowestBound = new float[h];
-            for(int line=0; line<splittedContent.Length; line++) {
+            for(int line = 0; line < splittedContent.Length; line++) {
                 List<float> numbers = splittedContent[line]
                     .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
                     .ToList()
@@ -61,6 +61,7 @@ namespace BranchAndBound
                 int i = 0;
                 numbers.ForEach((v) => TSPMatrix[i++, line] = v);
             }
+            originalTSPMatrix = (float[,])TSPMatrix.Clone();
             appendLog("Finished parsing");
             PaintTSPMatrix();
             appendLog("Painted TSP Matrix");
@@ -70,8 +71,10 @@ namespace BranchAndBound
 
         private void toolStripButtonStep_Click(object sender, EventArgs e) {
             if(TSPMatrix == null) return;
+            appendLog(currentStatus.ToString() + "()");
             switch(currentStatus) {
-                case CurrentStatus.None:
+                case CurrentStatus.LoopStart:
+                    LoopStart();
                     break;
                 case CurrentStatus.FindLowestBound:
                     FindLowestBound();
@@ -88,12 +91,16 @@ namespace BranchAndBound
                 case CurrentStatus.Finish:
                     return;
             }
-            currentStatus = (CurrentStatus)((int)++currentStatus % (int)CurrentStatus.Finish);
+            PaintTSPMatrix();
+            if(currentStatus != CurrentStatus.Finish)
+                currentStatus = (CurrentStatus)((int)++currentStatus % (int)CurrentStatus.Finish);
         }
 
         private void appendLog(string s) {
             richTextBox1.AppendText(string.Format("{0}\n", s));
-            richTextBox1.SelectionStart = richTextBox1.TextLength;
+            richTextBox1.SelectionStart = richTextBox1.TextLength-1;
+            richTextBox1.SelectionLength = 1;
+            richTextBox1.ScrollToCaret();
         }
         private void PaintTSPMatrix() {
             const int cellSize = 32;
@@ -102,28 +109,30 @@ namespace BranchAndBound
             if(TSPMatrix == null) return;
             List<int> skippedRow = new List<int>();
             List<int> skippedCol = new List<int>();
-/*            for(int x = 0; x < TSPMatrix.GetLength(0); x++) {
+            for(int x = 0; x < TSPMatrix.GetLength(0); x++) {
                 if(Enumerable.Range(0, TSPMatrix.GetLength(1))
                     .Select(
-                        (y) => TSPMatrix[x, y] == float.PositiveInfinity
-                    ).Count() == TSPMatrix.GetLength(1))
-                    skippedCol.Add(x);
+                        (y) => TSPMatrix[x, y]
+                    ).Where( e => e == float.PositiveInfinity)
+                    .Count() == TSPMatrix.GetLength(1))
+                    skippedRow.Add(x);
             }
             for(int y = 0; y < TSPMatrix.GetLength(1); y++) {
                 if(Enumerable.Range(0, TSPMatrix.GetLength(0))
                     .Select(
-                        (x) => TSPMatrix[x, y] == float.PositiveInfinity
-                    ).Count() == TSPMatrix.GetLength(1))
+                        (x) => TSPMatrix[x, y]
+                    ).Where(e => e == float.PositiveInfinity)
+                    .Count() == TSPMatrix.GetLength(0))
                     skippedCol.Add(y);
-            }*/
+            }
             var bmp = new Bitmap(TSPMatrix.GetLength(0) * cellSize + cellSize, TSPMatrix.GetLength(1) * cellSize + cellSize);
             var g = Graphics.FromImage(bmp);
             g.Clear(Color.White);
             for(int y = 0; y < TSPMatrix.GetLength(1); y++) {
-                //if(skippedCol.Contains(y)) continue;
                 for(int x = 0; x < TSPMatrix.GetLength(0); x++) {
-                    //if(skippedRow.Contains(x)) continue;
                     g.DrawRectangle(Pens.Black, new Rectangle(x * cellSize, y * cellSize, cellSize, cellSize));
+                    if(skippedRow.Contains(x)) continue;
+                    if(skippedCol.Contains(y)) continue;
                     var sSize = g.MeasureString(TSPMatrix[x, y].ToString(), cellFont);
                     g.DrawString(
                         TSPMatrix[x, y].ToString(),
@@ -139,6 +148,30 @@ namespace BranchAndBound
             bmp.Dispose();
         }
 
+        private void LoopStart() {
+            // check if matrix is empty
+            Console.WriteLine("TSP infinity = {0}", TSPMatrix.Cast<float>().Where(e => float.IsInfinity(e)).Count());
+            TSPMatrix.Cast<float>().Where(e => float.IsInfinity(e)).Count();
+            if(TSPMatrix.Cast<float>().Where(e => float.IsInfinity(e)).Count() == TSPMatrix.GetLength(0) * TSPMatrix.GetLength(1)) {
+                currentStatus = CurrentStatus.Finish;
+                StringBuilder sb = new StringBuilder("Solution: ");
+                Dictionary<int, int> swappedSolution = solution.ToDictionary(v => v.Value, k => k.Key);
+                int nextValue = swappedSolution.Keys.Min();
+                sb.Append(nextValue);
+                float totalCost = 0;
+                while(swappedSolution.Count > 0) {
+                    sb.AppendFormat("->{0}", swappedSolution[nextValue] + 1);
+                    TSPMatrix[swappedSolution[nextValue], nextValue] = originalTSPMatrix[swappedSolution[nextValue], nextValue];
+                    totalCost += TSPMatrix[swappedSolution[nextValue], nextValue];
+                    var tmp = swappedSolution[nextValue];
+                    swappedSolution.Remove(nextValue);
+                    nextValue = tmp;
+                }
+                appendLog(sb.ToString());
+                appendLog(string.Format("Total cost: {0}", totalCost));
+            }
+
+        }
         private void FindLowestBound() {
             float[,] tempTSPMatrix = (float[,])TSPMatrix.Clone();
             for(int y = 0; y < tempTSPMatrix.GetLength(1); y++) {
@@ -147,7 +180,8 @@ namespace BranchAndBound
                                         (x) => tempTSPMatrix[x, y]
                                     ).Min();
                 for(int x = 0; x < tempTSPMatrix.GetLength(0); x++)
-                    tempTSPMatrix[x, y] -= yLowestBound[y];
+                    if(!float.IsInfinity(tempTSPMatrix[x, y])) tempTSPMatrix[x, y] -= yLowestBound[y];
+                appendLog(string.Format("LB for Y:{0}={1}", y, yLowestBound[y]));
             }
 
             for(int x = 0; x < tempTSPMatrix.GetLength(0); x++) {
@@ -156,7 +190,8 @@ namespace BranchAndBound
                                         (y) => tempTSPMatrix[x, y]
                                     ).Min();
                 for(int y = 0; y < tempTSPMatrix.GetLength(1); y++)
-                    tempTSPMatrix[x, y] -= yLowestBound[y];
+                    if(!float.IsInfinity(tempTSPMatrix[x, y])) tempTSPMatrix[x, y] -= xLowestBound[x];
+                appendLog(string.Format("LB for X:{0}={1}", x, xLowestBound[x]));
             }
             nextTSPMatrix = tempTSPMatrix;
         }
@@ -167,22 +202,51 @@ namespace BranchAndBound
         private void FindMaximumSum() {
             var indexOfZero = GetIndex(TSPMatrix, 0);
             List<float> values = new List<float>();
-            
+            float maxSum = float.MinValue;
+            Point maxCoord = new Point(-1, -1);
+            if(indexOfZero.Count > 0) maxCoord = indexOfZero[0];
             foreach(var t in indexOfZero) {
-                values.Add(GetSumOfMinimumValue(TSPMatrix, t));
+                var result = GetMinimumValue(TSPMatrix, t);
+                if(float.IsInfinity(result.Item1) || float.IsInfinity(result.Item2)) {
+                    appendLog(string.Format("No solution on ({0},{1})", t.X, t.Y));
+                    if(!solution.ContainsKey(t.X)) solution.Add(t.X, t.Y);
+                    TSPMatrix[t.X, t.Y] = float.PositiveInfinity;
+                    maxCoord = new Point(-1, -1);
+                    break;
+                }
+                float sum = result.Item1 + result.Item2;
+                appendLog(string.Format("({0}, {1}) {2} + {3} = {4}", t.X, t.Y, result.Item1, result.Item2, sum));
+                if(sum > maxSum) {
+                    maxSum = sum;
+                    maxCoord = t;
+                }
             }
+            if(maxCoord.X != -1)
+                coordinateReadyToRemove = maxCoord;
+
         }
         private void MakeColRowInfinite() {
+            if(coordinateReadyToRemove.X == -1) {
+                return;
+            }
+            appendLog(string.Format("Removing {0}", coordinateReadyToRemove));
+            solution.Add(coordinateReadyToRemove.X, coordinateReadyToRemove.Y);
 
+            for(int x = 0; x < TSPMatrix.GetLength(0); x++) 
+                TSPMatrix[x, coordinateReadyToRemove.Y] = float.PositiveInfinity;
+            for(int y = 0; y < TSPMatrix.GetLength(1); y++) 
+                TSPMatrix[coordinateReadyToRemove.X, y] = float.PositiveInfinity;
+            TSPMatrix[coordinateReadyToRemove.Y, coordinateReadyToRemove.X] = float.PositiveInfinity;
+            coordinateReadyToRemove = new Point(-1, -1);
         }
 
         List<Point> GetIndex(float[,] matrix, float value) {
             List<Point> points = new List<Point>();
-            for(int y = 0; y < matrix.GetLength(1); y++) 
-                for(int x = 0; x < matrix.GetLength(0); x++) 
-                    points.Add(new Point(x, y));
+            for(int y = 0; y < matrix.GetLength(1); y++)
+                for(int x = 0; x < matrix.GetLength(0); x++)
+                    if(matrix[x,y] == value) points.Add(new Point(x, y));
             return points;
-            
+
         }
         float GetSumOfMinimumValue(float[,] matrix, int x, int y) {
             return
@@ -200,4 +264,25 @@ namespace BranchAndBound
         float GetSumOfMinimumValue(float[,] matrix, Point p) {
             return GetSumOfMinimumValue(matrix, p.X, p.Y);
         }
+
+        Tuple<float, float> GetMinimumValue(float[,] matrix, int x, int y) {
+            return new Tuple<float, float>(
+                Enumerable.Range(0, matrix.GetLength(0))
+                .Where(_x => x != _x)
+                .Select(
+                    _x => matrix[_x, y]
+                ).Min()
+                ,
+                Enumerable.Range(0, matrix.GetLength(1))
+                .Where(_y => y != _y)
+                .Select(
+                    _y => matrix[x, _y]
+                ).Min()
+            );
+
+        }
+        Tuple<float, float> GetMinimumValue(float[,] matrix, Point p) {
+            return GetMinimumValue(matrix, p.X, p.Y);
+        }
+    }
 }
